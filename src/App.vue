c8d80f59a74f368a252317f45cefb7e872a22e77
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-04-01 17:41:24 +0200

    forgot to push

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <Header @searchInput="filmQuery" />
-
-    <FilmsList />
+    <Header @searchInput="getNewSearch" />
+    <!-- <div v-for="(film, index) in filmsList" :key="index">{{ film.title }}</div> -->
+    <FilmsList :filmsList="filmsList" />
   </div>
 </template>
 
@@ -19,28 +19,29 @@ export default {
   },
   data: function () {
     return {
+      //moviesApi: `https://api.themoviedb.org/3/search/movie?api_key=${process.env.VUE_APP_APIKEY}&language=en-US&page=1&query=`,
       filmsList: "",
       searchToInput: "",
     };
   },
-  created: function () {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.VUE_APP_APIKEY}&language=en-US&page=1&query='adult'`
-      )
-      .then((result) => {
-        this.filmsList = result.data.results;
-        console.log(this.filmsList);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
+  /* created: function () {
+    this.getNewSearch(this.moviesApi, this.searchToInput);
+  }, */
   methods: {
-    filmQuery(query) {
+    getNewSearch(query) {
+      const urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.VUE_APP_APIKEY}&language=en-US&page=1&query=${query}`;
+      axios
+        .get(urlMovie)
+        .then((result) => {
+          this.filmsList = result.data.results;
+          console.log(this.filmsList);
+        })
+        .catch((error) => console.error(error));
+    },
+    /* filmQuery(query) {
       this.searchToInput = query;
       console.warn(this.searchToInput);
-    },
+    }, */
   },
 };
 </script>
--- src/components/FilmsList.vue
+++ src/components/FilmsList.vue
@@ -5,6 +5,7 @@
 <script>
 export default {
   name: "FilmsList",
+  props: {},
 };
 </script>
 
