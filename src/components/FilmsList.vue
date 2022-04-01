145f6983bc616847f18e9d3999c6e5767bd6346e
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-04-01 17:49:29 +0200

    problem with check prop string to obj

--- src/components/FilmsList.vue
+++ src/components/FilmsList.vue
@@ -1,11 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li v-for="(film, index) in filmsList" :key="index">
+        {{ film.title }} - {{ film.original_title }} - {{ film.vote_average }} -
+        {{ film.original_language }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "FilmsList",
-  props: {},
+  props: {
+    filmsList: String || Object, //problemi di comunicazione
+  },
 };
 </script>
 
