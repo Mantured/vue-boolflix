002a9c3198ccf24ea43918c2419d8f084874d317
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-04-04 15:00:20 +0200

    added hover

--- src/components/ProductCover.vue
+++ src/components/ProductCover.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="card my-size mb-3" style="width: 20rem">
+  <div class="card my-size mb-3 position-relative" style="width: 20rem">
     <img :src="imgUrl(info.poster_path)" class="card-img-top" alt="" />
     <!-- <img src="" class="card-img-top" alt="" /> -->
-    <div class="card-body">
-      <h5 class="card-title">{{ info.title || info.name }}</h5>
+    <div class="card-body position-absolute">
+      <h5 class="card-title">Titolo : {{ info.title || info.name }}</h5>
       <!-- <h5 class="card-title">{{ info.name }}</h5> -->
       <p class="card-text">
-        {{ info.original_title || info.original_name }}
+        Titolo originale : {{ info.original_title || info.original_name }}
         <!-- {{ info.original_name }} -->
       </p>
       <country-flag :country="getFlag(info.original_language)" size="normal" />
-      <p class="card-text">{{ voteInt(info.vote_average) }}</p>
       <span v-for="i in voteInt(info.vote_average)" :key="i + 'abc1'">
         <font-awesome-icon class="yellow" icon="fa-solid fa-star" />
       </span>
@@ -39,6 +38,7 @@ export default {
       if (lang === "en") initials = "gb";
       else if (lang === "ko") initials = "kp";
       else if (lang === "ja") initials = "jp";
+      else if (lang === "zh") initials = "cn";
       else initials = lang;
       return initials;
     },
@@ -58,8 +58,17 @@ export default {
   img {
     height: 100%;
   }
+  .card-body {
+    display: none;
+  }
 }
-.yellow {
-  color: rgb(239, 212, 11);
+.card:hover .card-body {
+  display: block;
+  height: 100%;
+  background-color: rgb(20, 20, 20);
+  color: white;
+  .yellow {
+    color: rgb(239, 212, 11);
+  }
 }
 </style>
